5a23b424e774fcbea730d16464b539fb6a9a3231
YosefGu <yos770yos@gmail.com>
2025-10-21 12:54:24 +0300

    update to pandas dataframe

--- Business_logic/file_handling.py
+++ Business_logic/file_handling.py
@@ -1,29 +1,45 @@
+import pandas as pd
 from pymavlink import mavutil
 
-class FileHandling():
 
-    def __init__(self, path):
+class FileHandling:
+
+    def __init__(self, path: str):
         self.path = path
 
-    def run(self):
-        points = self.extarct_coords_from_bin_file()
-        # print(len(points))
-        updateted_points = self.removing_nearby_points(points)
-        # print(len(updateted_points))
-        return updateted_points
-    
-    def extarct_coords_from_bin_file(self):
+    def run(self) -> pd.DataFrame:
+        df_points = self._extarct_coords_from_bin_file()
+        df_points = self._drop_duplicait(df_points)
+        df_points = self._removing_nearby_points(df_points, 40)
+        return df_points
+
+    def _extarct_coords_from_bin_file(self) -> pd.DataFrame:
         mav = mavutil.mavlink_connection(self.path)
-        points = []
+        betch = []
+        dfs = []
         while True:
-            msg = mav.recv_match(type='GPS', blocking=False)  
+            msg = mav.recv_match(type="GPS", blocking=False)
             if msg is None:
                 break
             if hasattr(msg, "I") and msg.I == 1:
-                points.append((msg.Lat, msg.Lng))
-        return points
+                betch.append((msg.Lat, msg.Lng))
+            if len(betch) >= 10_000:
+                dfs.append(pd.DataFrame(betch, columns=["Lat", "Lng"]))
+                betch.clear()
+        if betch:
+            dfs.append(pd.DataFrame(betch, columns=["Lat", "Lng"]))
 
-    def removing_nearby_points(self, points):
-        return points[::40]
-      
+        df = pd.concat(dfs, ignore_index=True)
+        return df
 
+    def _drop_duplicait(self, df_points: pd.DataFrame) -> pd.DataFrame:
+        shifted = df_points.shift()
+        comparison = df_points == shifted
+        same_as_prev = comparison.all(axis=1)
+        filterd = df_points.loc[~same_as_prev]
+        filterd.reset_index(drop=True)
+        return filterd
+
+    def _removing_nearby_points(self, df_points: pd.DataFrame, n: int) -> pd.DataFrame:
+        df_reduced = df_points.iloc[::n].reset_index(drop=True)
+        return df_reduced
