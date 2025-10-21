d802bc6e608a0f87f0689b85f43319148941526a
YosefGu <yos770yos@gmail.com>
2025-10-21 12:53:12 +0300

    update logic and UI

--- GUI/home_screen.py
+++ GUI/home_screen.py
@@ -1,71 +1,71 @@
 import flet as ft
 
 
-
 class HomeScreen:
-    def __init__(self, page: ft.Page, set_path):
-        self.page = page
-        self.set_path = set_path
-        self.message = ft.Text("", size=16)
-        self.container = self.build()
 
-    def build(self):
-        title = ft.Text("Welcome", size=34, weight=ft.FontWeight.BOLD)
+    def __init__(self, page: ft.Page):
+        self.page = page
+        self.message = ft.Text("", size=16)
+        self.container = self._build()
+
+    def _build(self) -> ft.Container:
+        title = ft.Text("Welcome", size=34, weight=ft.FontWeight.BOLD, color="#3383C4")
+
         description = ft.Text(
             "Select a flight log file (.bin) to analyze and view the flight path on the map.",
-            size=20,
-            color=ft.Colors.GREY_800,
+            size=18,
+            color=ft.Colors.GREY_700,
+            text_align=ft.TextAlign.CENTER,
         )
 
         choose_btn = ft.ElevatedButton(
             content=ft.Row(
-                [
-                    ft.Text("Choose file", size=16),
-                    ft.Icon(ft.Icons.UPLOAD_FILE)
-                ],
+                [ft.Text("Choose file", size=16, weight=ft.FontWeight.W_500), ft.Icon(ft.Icons.UPLOAD_FILE)],
                 alignment=ft.MainAxisAlignment.CENTER,
-            )
-           ,
-            width=150,
+                spacing=10,
+            ),
+            width=160,
             height=50,
-            on_click=lambda e: self.pick_file(),
+            style=ft.ButtonStyle(
+                bgcolor=ft.Colors.BLUE_600,
+                color=ft.Colors.WHITE,
+                shape=ft.RoundedRectangleBorder(radius=10),
+                overlay_color=ft.Colors.BLUE_400,
+            ),
+            on_click=lambda e: self._pick_file(),
         )
 
-        card = ft.Card(
-            content=ft.Container(
-                content=ft.Column(
-                    [title, description, ft.Divider(), self.message, choose_btn],
-                    spacing=20,
-                    horizontal_alignment=ft.CrossAxisAlignment.CENTER,
-                ),
-                padding=100,
-                margin=50,
-                border_radius=15,
-                bgcolor="#B6D5DE",
-                shadow=ft.BoxShadow(blur_radius=8, spread_radius=2, color=ft.Colors.GREY_200),
-            )
+        container = ft.Container(
+            content=ft.Column(
+                [title, description, ft.Divider(), self.message, choose_btn],
+                spacing=20,
+                horizontal_alignment=ft.CrossAxisAlignment.CENTER,
+            ),
+            padding=100,
+            margin=75,
+            border_radius=15,
+            bgcolor="#B6D5DE",
+            shadow=ft.BoxShadow(blur_radius=8, spread_radius=2, color=ft.Colors.GREY_200),
         )
-
 
         return ft.Container(
-            content=card,
+            content=container,
             expand=True,
             alignment=ft.alignment.center,
-            bgcolor=ft.Colors.BLUE_GREY_50,
+            bgcolor=ft.Colors.RED_50,
         )
 
     # Logic
-    def pick_file(self):
-        file_picker = ft.FilePicker(on_result=self.on_file_picked)
+    def _pick_file(self) -> None:
+        file_picker = ft.FilePicker(on_result=self._on_file_picked)
         self.page.overlay.append(file_picker)
         self.page.update()
         file_picker.pick_files(allowed_extensions=["bin"])
 
-    def on_file_picked(self, e: ft.FilePickerResultEvent):
+    def _on_file_picked(self, e: ft.FilePickerResultEvent) -> None:
         if e.files:
-            self.set_path(e.files[0].path)
-            self.page.go('/map')
+            self.page.client_storage.set("path", e.files[0].path)
+            self.page.go("/map")
         else:
             self.message.value = "No file selected."
             self.page.update()
-
